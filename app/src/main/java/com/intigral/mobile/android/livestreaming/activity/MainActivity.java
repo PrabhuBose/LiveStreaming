b7305e4047f94832d72e05cae76f36945477aaf0
System Administrator <root@Senthils-MacBook-Pro.local>
2018-06-26 13:27:12 +0400

    Changed video view to mediaplayer with mediacontroller

--- app/src/main/java/com/intigral/mobile/android/livestreaming/activity/MainActivity.java
+++ app/src/main/java/com/intigral/mobile/android/livestreaming/activity/MainActivity.java
@@ -1,20 +1,172 @@
 package com.intigral.mobile.android.livestreaming.activity;
 
-import android.net.Uri;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.VideoView;
+import android.util.DisplayMetrics;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.MediaController;
+import android.widget.RelativeLayout;
 
 import com.intigral.mobile.android.livestreaming.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SurfaceHolder.Callback,
+        MediaPlayer.OnPreparedListener, MediaController.MediaPlayerControl {
+
+    private MediaPlayer mediaPlayer;
+    private SurfaceHolder vidHolder;
+    private SurfaceView vidSurface;
+    private MediaController mediaController;
+    private Handler handler = new Handler();
+    private RelativeLayout videoLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        VideoView vidView = findViewById(R.id.myVideo);
-        vidView.setVideoURI(Uri.parse(getResources().getString(R.string.streaming_url)));
-        vidView.start();
+        mediaController = new MediaController(this);
+        videoLayout = findViewById(R.id.videoLayout);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        this.getWindowManager().getDefaultDisplay().getMetrics(dm);
+        int height = dm.heightPixels;
+        int width = dm.widthPixels;
+
+        int left = videoLayout.getLeft();
+        int top = videoLayout.getTop();
+        int right = left + (width / 4);
+        int bottom = top + (height / 4);
+
+        videoLayout.layout(left, top, right, bottom);
+
+        vidSurface = findViewById(R.id.surfView);
+        vidHolder = vidSurface.getHolder();
+        vidHolder.addCallback(this);
+    }
+
+    @Override
+    public void onPrepared(MediaPlayer mediaPlayer) {
+        mediaPlayer.start();
+
+        mediaController.setMediaPlayer(this);
+        mediaController.setAnchorView(vidSurface);
+        handler.post(new Runnable() {
+
+            public void run() {
+                mediaController.setEnabled(true);
+                mediaController.show();
+            }
+        });
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder surfaceHolder) {
+
+        try {
+            mediaPlayer = new MediaPlayer();
+            mediaPlayer.setDisplay(vidHolder);
+            mediaPlayer.setDataSource(getResources().getString(R.string.streaming_url));
+            mediaPlayer.prepare();
+            mediaPlayer.setOnPreparedListener(this);
+            mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mediaPlayer.pause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+//        mediaPlayer.start();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mediaPlayer.stop();
+    }
+
+    @Override
+    public void start() {
+        mediaPlayer.start();
+    }
+
+    @Override
+    public void pause() {
+        mediaPlayer.pause();
+    }
+
+    @Override
+    public int getDuration() {
+        return mediaPlayer.getDuration();
+    }
+
+    @Override
+    public int getCurrentPosition() {
+        return mediaPlayer.getCurrentPosition();
+    }
+
+    @Override
+    public void seekTo(int i) {
+        mediaPlayer.seekTo(i);
+
+    }
+
+    @Override
+    public boolean isPlaying() {
+        return mediaPlayer.isPlaying();
+    }
+
+    @Override
+    public int getBufferPercentage() {
+        return 0;
+    }
+
+    @Override
+    public boolean canPause() {
+        return true;
+    }
+
+    @Override
+    public boolean canSeekBackward() {
+        return false;
+    }
+
+    @Override
+    public boolean canSeekForward() {
+        return true;
+    }
+
+    @Override
+    public int getAudioSessionId() {
+        return 0;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        mediaController.show();
+        return false;
     }
 }
