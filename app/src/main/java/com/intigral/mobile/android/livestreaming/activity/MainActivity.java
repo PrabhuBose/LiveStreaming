cf955c6eb78ac591a13e4a69d472ba843f460a8d
System Administrator <root@Senthils-MacBook-Pro.local>
2018-06-26 12:12:16 +0400

    Video View added

--- app/src/main/java/com/intigral/mobile/android/livestreaming/activity/MainActivity.java
+++ app/src/main/java/com/intigral/mobile/android/livestreaming/activity/MainActivity.java
@@ -1,7 +1,11 @@
-package com.intigral.mobile.android.livestreaming;
+package com.intigral.mobile.android.livestreaming.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.VideoView;
+
+import com.intigral.mobile.android.livestreaming.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        VideoView vidView = findViewById(R.id.myVideo);
+        vidView.setVideoURI(Uri.parse(getResources().getString(R.string.streaming_url)));
+        vidView.start();
     }
 }
